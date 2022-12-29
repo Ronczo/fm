a80a6170eb439921dbee8ce6e1f9ea9023bef225
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 18:10:44 +0100

    add Image model

---
+++ images/models.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    width = models.PositiveSmallIntegerField()
+    height = models.PositiveSmallIntegerField()
