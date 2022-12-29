40993bc1bd3af5387c06fe0272202f40bceb810c
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 22:37:01 +0100

    add AWS credentials into settings

--- fm/settings.py
+++ fm/settings.py
@@ -138,3 +138,10 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 5,
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend")
 }
+
+# S3 settings
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_QUERYSTRING_AUTH = False
