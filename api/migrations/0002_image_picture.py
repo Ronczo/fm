715aa4191c82b660dc3349d9a326e1f9e9d75962
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 21:18:08 +0100

    migration refactor

--- api/migrations/0002_image_picture.py
+++ api/migrations/0002_image_picture.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name="image",
+            model_name="Image",
             name="picture",
             field=models.FileField(blank=True, null=True, upload_to="pictures/"),
         ),
