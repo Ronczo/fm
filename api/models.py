5f4e06737f0585053b987efb7f657d2491069b73
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 21:10:12 +0100

    change url field in model Image

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Image(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    url = models.URLField()
+    url = models.FileField(upload_to='pictures/')
     title = models.CharField(max_length=255)
     width = models.PositiveSmallIntegerField()
     height = models.PositiveSmallIntegerField()
