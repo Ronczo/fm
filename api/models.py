13c38698521a91bf6639d983f8cf5cfa0ddac801
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 22:36:43 +0100

    change field picture from FileField into ImageField

--- api/models.py
+++ api/models.py
@@ -3,7 +3,13 @@ from django.db import models
 
 class Image(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    picture = models.FileField(upload_to='pictures/', null=True, blank=True)
+    picture = models.ImageField(upload_to='pictures/', null=True, blank=True)
     title = models.CharField(max_length=255)
     width = models.PositiveSmallIntegerField()
     height = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.title} {self.width}x{self.height}"
+    @property
+    def get_s3_url(self):
+        return self.picture.url
