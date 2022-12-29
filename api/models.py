ed82e4c1a9d1d3ca12b69c2b3c77cf065bbe3b8b
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 21:14:06 +0100

    update model Image

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Image(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    url = models.FileField(upload_to='pictures/')
+    picture = models.FileField(upload_to='pictures/', null=True, blank=True)
     title = models.CharField(max_length=255)
     width = models.PositiveSmallIntegerField()
     height = models.PositiveSmallIntegerField()
