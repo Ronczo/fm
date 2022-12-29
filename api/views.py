b1d2469963c450adca31f0afb4f637e8576890e9
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 19:46:07 +0100

    add view set for images (WIP)

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import mixins, generics
 
-# Create your views here.
+from api.filters import ImageFilter
+from api.serializers import ImageSerializer
+from api.models import Image
+from django_filters import rest_framework
+
+class ImageViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = ImageSerializer
+    queryset = Image.objects.all()
+    filter_backends = (rest_framework.DjangoFilterBackend,)
+    filterset_fields = ('title',)
+
+    def get(self, request, *args, **kwargs):
+
+        if kwargs.get("pk"):
+            return self.retrieve(request, *args, **kwargs)
+        else:
+            return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        ...
