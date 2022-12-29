b591d59e36cd14649b52c77dde510dbb70577f7a
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 18:20:17 +0100

    add serializer for Image model

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,9 @@
 from rest_framework import serializers
+from .models import Image
 
-class ImageSerializer(serializers.Serializer):
-    ...
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = "__all__"
+        ref_name = "CImage serializer"
+
