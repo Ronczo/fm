b7cbbfeea57d34c8fd68a9178cd32e54759e82b3
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 22:37:24 +0100

    add post method to Image view set

--- api/views.py
+++ api/views.py
@@ -1,22 +1,29 @@
+from django_filters import rest_framework
 from rest_framework import mixins, generics
+from rest_framework.response import Response
 
 from api.filters import ImageFilter
-from api.serializers import ImageSerializer
 from api.models import Image
-from django_filters import rest_framework
+from api.serializers import ImageSerializer
+
+
 
 class ImageViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
     serializer_class = ImageSerializer
     queryset = Image.objects.all()
     filter_backends = (rest_framework.DjangoFilterBackend,)
+    filterset_class = ImageFilter # I think it's not necessary
     filterset_fields = ('title',)
 
-    def get(self, request, *args, **kwargs):
 
+    def get(self, request, *args, **kwargs):
         if kwargs.get("pk"):
             return self.retrieve(request, *args, **kwargs)
         else:
             return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        ...
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("The image has been added")
