220ad6e1dbd8a7d3b59c27bb6def40ed4826135e
Marcin Jarosińśki <jarosinski91@gmail.com>
2022-12-29 17:57:03 +0100

    update debug field in settings

--- fm/settings.py
+++ fm/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
