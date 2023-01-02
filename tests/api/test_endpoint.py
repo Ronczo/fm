40be8c9c7355acbb946cce4dc9fc223f0db0776d
Marcin Jarosińśki <jarosinski91@gmail.com>
2023-01-02 12:59:04 +0100

    add endpoint tests update fixtures

---
+++ tests/api/test_endpoint.py
@@ -0,0 +1,75 @@
+
+from typing import List, Dict, Union
+
+import pytest
+from django.db.models.fields.files import ImageFieldFile
+from rest_framework.response import Response
+
+from django.conf import settings
+
+from api.models import Image
+
+
+@pytest.mark.django_db
+def test_fetching_list(db, client):
+    response: Response = client.get("/api/images/")
+    assert response.status_code == 200
+    expected_fields: List[str] = ["count", "next", "previous", "results"]
+    assert all(
+        field in response.data.keys() for field in expected_fields
+    )  # check pagination
+    assert (
+        len(response.data["results"]) == settings.REST_FRAMEWORK["PAGE_SIZE"]
+    )  # check pagination size
+    expected_fields_in_object: List[str] = ["id", "url", "title", "width", "height"]
+    # Check object structure
+    for obj in response.data["results"]:
+        assert all(field in obj for field in expected_fields_in_object)
+
+
+@pytest.mark.django_db
+def test_fetching_retrieve(db, client, first_image):
+    response: Response = client.get(f"/api/images/{first_image.id}/")
+
+    assert response.status_code == 200
+    expected_fields: List[str] = ["id", "url", "title", "width", "height"]
+    assert all(field in response.data.keys() for field in expected_fields)
+
+    payload: Dict[str, Union[str, int]] = {
+        "id": first_image.id,
+        "url": first_image.picture.url,
+        "title": first_image.title,
+        "width": first_image.width,
+        "height": first_image.height,
+    }
+    assert sorted(payload) == sorted(response.data)
+
+
+@pytest.mark.django_db
+def test_post_image(db, client, build_image):
+    image_amount_in_db: int = Image.objects.all().count()
+    payload: Dict[str, Union[str, int, ImageFieldFile]] = {
+        "title": build_image.title,
+        "width": build_image.width,
+        "height": build_image.height,
+        "picture": build_image.picture,
+    }
+    response: Response = client.post(f"/api/images/", payload)
+
+    image_amount_in_db_after_post_request: int = Image.objects.all().count()
+    #  # image added to db
+    assert image_amount_in_db_after_post_request == image_amount_in_db + 1
+
+    # check object structure
+    expected_fields: List[str] = ["id", "url", "title", "width", "height"]
+    assert all(
+        field in response.data.keys() for field in expected_fields
+    )
+
+    # check response values
+    for key, value in payload.items():
+        if key != "picture":
+            assert value == response.data[key]
+
+
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,11 @@ import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
+from api.models import Image
 from tests.factories import ImageFactory
 
 register(ImageFactory)
+
 
 @pytest.fixture
 def client():
@@ -17,6 +19,10 @@ def db(image_factory):
     for _ in range(20):
         image_factory.create()
 
+@pytest.fixture
+def first_image():
+    return Image.objects.first()
 
-
-
+@pytest.fixture
+def build_image(image_factory):
+    return image_factory.build()
