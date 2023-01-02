6d0e986fab33d564ed469ce3016b28e14f477e00
Marcin Jarosińśki <jarosinski91@gmail.com>
2023-01-02 14:19:45 +0100

    add heigt and width parameter to picture field in ImageFactory

--- tests/factories.py
+++ tests/factories.py
@@ -10,4 +10,4 @@ class ImageFactory(factory.django.DjangoModelFactory):
     width: int = randint(100, 300)
     height: int = randint(100, 300)
     title: str = f"test {width} {height}"
-    picture = factory.django.ImageField(color='blue')
+    picture = factory.django.ImageField(color='blue', height=300, width=300)
