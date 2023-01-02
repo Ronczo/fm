c86f747528f1bece400168eacf887a3a16689540
Marcin Jarosińśki <jarosinski91@gmail.com>
2023-01-02 11:56:31 +0100

    conftest, factories, test settings (WIP)

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+from pytest_factoryboy import register
+from rest_framework.test import APIClient
+
+from tests.factories import ImageFactory
+
+register(ImageFactory)
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def db(image_factory):
+    for _ in range(20):
+        image = image_factory.create()
+
+
+
+
---
+++ tests/factories.py
@@ -0,0 +1,13 @@
+from random import randint
+import factory
+from api.models import Image
+
+
+class ImageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Image
+
+    width: int = randint(100, 300)
+    height: int = randint(100, 300)
+    title: str = f"test {width} {height}"
+    picture = factory.django.ImageField(color='blue')
