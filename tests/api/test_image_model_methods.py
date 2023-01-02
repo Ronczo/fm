a8acdf4648168eefc14c89b14f6c309f16d8f6a3
Marcin Jarosińśki <jarosinski91@gmail.com>
2023-01-02 14:18:55 +0100

    add tests to Image model

---
+++ tests/api/test_image_model_methods.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.mark.django_db
+def test_image_resize(create_image):
+    wanted_image_size: tuple = (create_image.width, create_image.height)
+    create_image.image_resize(create_image.picture)
+    assert (create_image.picture.width, create_image.picture.height) == wanted_image_size
+
+@pytest.mark.django_db
+def test_get_picture_url(create_image):
+    assert create_image.get_picture_url == create_image.picture.url
+
