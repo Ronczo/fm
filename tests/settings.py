57d048cb2108796695fe540a552fb7106fbf8758
Marcin Jarosińśki <jarosinski91@gmail.com>
2023-01-02 14:19:15 +0100

    detele storage from test settings

--- tests/settings.py
+++ tests/settings.py
@@ -142,4 +142,3 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend"),
     "DEFAULT_SCHEMA_CLASS": 'drf_spectacular.openapi.AutoSchema'
 }
-DEFAULT_FILE_STORAGE = 'inmemorystorage.InMemoryStorage'
